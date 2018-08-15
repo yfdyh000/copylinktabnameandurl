74951f0356296c0a25cee1cecc75a3e3509a6d12
YFdyh000 <yfdyh000@gmail.com>
2018-08-15 23:41:58 +0800

    cleanup, refine and turn to 'document_start'

--- background.js
+++ background.js
@@ -1,32 +1,4 @@
 /*
-Called when the item has been created, or when creation failed due to an error.
-We'll just log success/failure here.
-*/
-function onCreated(n) {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
-  } else {
-    //console.log("Item created successfully");
-  }
-}
-
-/*
-Called when the item has been removed.
-We'll just log success here.
-*/
-function onRemoved() {
-  //console.log("Item removed successfully");
-}
-
-/*
-Called when there was an error.
-We'll just log the error here.
-*/
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-/*
 Create all the context menu items.
 */
 browser.contextMenus.create({
@@ -46,8 +18,7 @@ function createContextMenuItemOnTab(info) {
     contexts: [onwhat]
   }, onCreated);
 }
-var gettingInfo = browser.runtime.getBrowserInfo();
-gettingInfo.then(createContextMenuItemOnTab);
+browser.runtime.getBrowserInfo().then(createContextMenuItemOnTab);
 
 
 
@@ -58,8 +29,7 @@ browser.runtime.onMessage.addListener(function (info) {
 
 function CopyOnLink(info,tab)
 {
-  var gettingItem = browser.storage.local.get('format');
-  gettingItem.then((res) => {
+  browser.storage.local.get('format').then((res) => {
     formatvalue = res.format || '%U %T';
     browser.tabs.sendMessage(tab.id, formatvalue.replace('%U',_linkinfo.url).replace('%T',_linkinfo.name));
   });
@@ -67,8 +37,7 @@ function CopyOnLink(info,tab)
 }
 function CopyOnTab(tab)
 {
-  var gettingItem = browser.storage.local.get('format');
-  gettingItem.then((res) => {
+  browser.storage.local.get('format').then((res) => {
     formatvalue = res.format || '%U %T';
     browser.tabs.sendMessage(tab.id, formatvalue.replace('%U',tab.url).replace('%T', tab.title));
   });
